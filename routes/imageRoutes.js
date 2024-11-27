fde6e45d19d944e9288f2420ff75ccbe2fbcf764
shoharto <bayes@graduate.utm.my>
2024-11-27 16:43:21 +0800

    feat: Add GET route for fetching all images

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { upload, uploadImage } = require('../controllers/imageController');
+const { upload, uploadImage, getAllImagesController } = require('../controllers/imageController');
 
 const router = express.Router();
 
 // POST route for uploading images
 router.post('/upload', upload, uploadImage);
+
+// GET route for fetching all images
+router.get('/', getAllImagesController);
 
 module.exports = router;
