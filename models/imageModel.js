6c8ea1274b08bc12432950d1e0d75cae0fce0d12
shoharto <bayes@graduate.utm.my>
2024-11-27 16:43:09 +0800

    feat: Update imageModel to include getAllImages function

--- models/imageModel.js
+++ models/imageModel.js
@@ -1,15 +1,26 @@
 const pool = require('../config/db');
 
+// Function to insert image path into the database
 const insertImagePath = async (imagePath) => {
   const query = 'INSERT INTO images(image_path) VALUES($1) RETURNING id, image_path';
   const values = [imagePath];
-
   try {
     const result = await pool.query(query, values);
-    return result.rows[0]; // Return the inserted row
+    return result.rows[0];
   } catch (error) {
     throw new Error('Database error: ' + error.message);
   }
 };
 
-module.exports = { insertImagePath };
+// Function to get all image paths from the database
+const getAllImages = async () => {
+  const query = 'SELECT * FROM images';
+  try {
+    const result = await pool.query(query);
+    return result.rows;
+  } catch (error) {
+    throw new Error('Database error: ' + error.message);
+  }
+};
+
+module.exports = { insertImagePath, getAllImages };
